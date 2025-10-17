783bb48528a6163b2f51ae7f4a943bd254bc5433
jedoGit <jedogit@gmail.com>
2025-10-16 22:33:48 -0400

    Skeleton Turnstile in chat route

--- RAG-App-Tool-Cloudflare/src/routes/chat.ts
+++ RAG-App-Tool-Cloudflare/src/routes/chat.ts
@@ -9,7 +9,39 @@ export const chatRoute = new Hono<{
 }>();
 
 chatRoute.post("/chat", async (c) => {
-  const { question } = await c.req.json();
+  const body = await c.req.json();
+  const turnstileToken = body["cf-turnstile-response"];
+  const question = body["question"];
+
+  // console.log(question)
+  // console.log(turnstileToken)
+
+  // Check if we receive the turnstile token
+  // if (!question || !turnstileToken) {
+  //   return c.json({ error: "Missing required field." }, 400);
+  // }
+
+  // Verify Turnstile Token
+  // const ip = c.req.header("CF-Connecting-IP") || "";
+
+  // console.log(ip)
+
+  // const formData = new FormData();
+  // formData.append("secret", String(env.TURNSTILE_SECRET_KEY));
+  // formData.append("response", String(turnstileToken));
+  // formData.append("remoteip", ip);
+
+  // const turnstileRes = await fetch("https://challenges.cloudflare.com/turnstile/v0/siteverify", {
+  //   method: "POST",
+  //   body: formData,
+  // });
+
+  // console.log(turnstileRes)
+
+  // const turnstileData = await turnstileRes.json<{ success: boolean }>();
+  // if (!turnstileData.success) {
+  //   return c.json({ error: "Failed Turnstile verification." }, 403);
+  // }
 
   // Create embedding to the question received from chat
   const queryEmbedding = await embedText(c.env.AI, question);
